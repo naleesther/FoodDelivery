999b93069af0adcc466d7e21f2331fbe83909200
naleesther <esthernalenyi@gmail.com>
2022-06-28 21:53:14 +0300

    first commit

---
+++ app/src/main/java/com/nalenyi/fooddelivery/Page2Activity.kt
@@ -0,0 +1,11 @@
+package com.nalenyi.fooddelivery
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Page2Activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_page2)
+    }
+}
---
+++ app/src/main/java/com/nalenyi/fooddelivery/SplashActivity.kt
@@ -0,0 +1,15 @@
+package com.nalenyi.fooddelivery
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        startActivity(Intent(this,Page2Activity::class.java))
+
+
+    }
+}
